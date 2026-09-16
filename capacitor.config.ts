4ccc4bd13c1d8ef4695c6d80eece724280dd0016
MiguelKing01 <mgaleanoc@uniempresarial.edu.co>
2026-09-15 23:54:51 -0500

    bre

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'co.edu.ue.qrbarras',
+  appName: 'lector-qr-barras',
+  webDir: 'dist/lector-qr-barras/browser'
+};
+
+export default config;
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,39 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CapacitorBarcodeScanner } from '@capacitor/barcode-scanner';
+import { IonButton, IonContent, IonHeader, IonIcon, IonTitle, IonToolbar } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, 
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonButton,
+    IonIcon
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+
   protected readonly title = signal('lector-qr-barras');
+
+  result: string = 'No has escaneado nada aun';
+
+  escanear = async () => {
+    try {
+        const response = await CapacitorBarcodeScanner.scanBarcode({
+        hint: 17
+      });
+
+      this.result = String(response.ScanResult);
+      console.log('Resultado:', this.result);
+
+    } catch (error) {
+      console.error('Error al escanear:', error);
+    }
+  }
+
 }
